c7a59dcf68f909412ae7eb71cd3f2633ccfd7cdb
Samuellekpor <samuellekpor@gmail.com>
2022-10-12 15:39:51 +0000

    Add multiplication test

--- calculator.test.js
+++ calculator.test.js
@@ -14,7 +14,7 @@ describe('Addition tests', () => {
   });
   
   test('The addition of 2 and 4', () => {
-    expect(added).toBeLessThan(10);
+    expect(calc3.addition()).toBe(19);
   })
 });
 
@@ -29,5 +29,33 @@ describe('Substraction tests', () => {
 
   test('Substraction of 10 and 9', () => {
     expect(calc3.substract()).toBe(1);
+  })
+});
+
+describe('Division tests', () => {
+  test('Division of 2 and 4', () => {
+    expect(calc.divide()).toBeCloseTo(0.5);
+  });
+
+  test('Division of 8 and 6', () => {
+    expect(calc2.divide()).toBeCloseTo(1.333);
+  });
+
+  test('Division of 10 and 9', () => {
+    expect(calc3.divide()).toBeCloseTo(1.111);
+  })
+});
+
+describe('Multiplication tests', () => {
+  test('Multiplication of 2 and 4', () => {
+    expect(calc.multiply()).toBe(8);
+  });
+
+  test('Multiplication of 8 and 6', () => {
+    expect(calc2.multiply()).toBe(48);
+  });
+
+  test('Mutiplication of 10 and 9', () => {
+    expect(calc3.multiply()).toBe(90);
   })
 })
